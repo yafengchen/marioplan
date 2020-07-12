1dd7665736363ca8dc6a6335e46d34138d1f17ea
Y Chen <yafeng_chen@outlook.com>
2020-07-13 03:43:53 +0800

    Dashboard Signed-off-by: Y Chen

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by XXX</p>
+                    <p className="grey-text">2020/7/13</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by XXX</p>
+                    <p className="grey-text">2020/7/13</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by XXX</p>
+                    <p className="grey-text">2020/7/13</p>
+                </div>
+            </div>                        
+
+        </div>
+    )
+}
+
+export default ProjectList
