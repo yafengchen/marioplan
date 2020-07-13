b434c1ca6bae7fa169fd859c120f8f14b357b4b0
Y Chen <yafeng_chen@outlook.com>
2020-07-14 03:08:26 +0800

    Sign In and Sign Up
    Signed-off-by: Y Chen

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+          <div className="card z-depth-0">
+              <div className="card-content">
+                  <span className="card-title">Project Title</span>
+                  <p>Lorem  ispum dolor sit amet</p>
+              </div>
+              <div className="card-action gret lighten-4 grey-text">
+                  <div>Posted by XXX</div>
+                  <div>2020/7/14</div>
+              </div>
+          </div>           
+        </div>
+    )
+}
+
+export default ProjectDetails
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by XXX</p>
+            <p className="grey-text">2020/7/13</p>
+        </div>
+        </div>   
+    )
+}
+
+export default ProjectSummary
