a389193059a41631900ff75a7173db97f08a730c
Y Chen <yafeng_chen@outlook.com>
2020-07-15 15:12:14 +0200

    dispatch Signed-off-by: Y Chen

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
-            <p>Posted by XXX</p>
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
             <p className="grey-text">2020/7/13</p>
         </div>
         </div>   
